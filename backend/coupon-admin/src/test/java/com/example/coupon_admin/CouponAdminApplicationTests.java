fa1475ed2465cfc0a5afefae04225ed6bdac38eb
jimizip <9koorry9@gmail.com>
2025-10-16 00:03:21 +0900

    초기 설정

---
+++ backend/coupon-admin/src/test/java/com/example/coupon_admin/CouponAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupon_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
