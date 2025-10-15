b2b61a2f44f8f476d30ffd86783e340e075eaf83
jimizip <9koorry9@gmail.com>
2025-10-15 23:59:14 +0900

    feat: repository

--- backend/coupon-admin/src/main/java/com/example/coupon_admin/repository/UploadFileRepository.java
+++ backend/coupon-admin/src/main/java/com/example/coupon_admin/repository/UploadFileRepository.java
@@ -1,4 +1,7 @@
 package com.example.coupon_admin.repository;
 
-public interface UploadFileRepository {
+import com.example.coupon_admin.domain.UploadFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
 }
